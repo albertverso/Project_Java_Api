4c8fa760a4e016f66ce86bdcff0eb6c6da402d1d
Carlos Alberto <caralbertverso@gmail.com>
2023-10-27 00:01:00 -0300

    Desafio DIO API

---
+++ src/main/java/projectjavaapi/projectjavaapi/model/ClienteRepository.java
@@ -0,0 +1,8 @@
+package projectjavaapi.projectjavaapi.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
---
+++ src/test/java/projectjavaapi/projectjavaapi/ProjectJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package projectjavaapi.projectjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
